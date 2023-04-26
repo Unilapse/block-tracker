4f924a39b2381d43a2118c70b8411c748773c7d4
jumyeong33 <jm01028250379@gmail.com>
2023-04-26 16:28:33 +0900

    [Add] mainnet contract address & node url

--- src/provider.js
+++ src/provider.js
@@ -7,7 +7,7 @@ const { writeErrorLogConnection } = require('./logAction');
 const chainEndpointMap = {
   klaytn: {
     endPoint: NETWORK === 'test' ? 'wss://node-api.klaytnapi.com/v1/ws/open?chain-id=1001' : 'wss://node-api.klaytnapi.com/v1/ws/open?chain-id=8217',
-    marketContractAddress: NETWORK === 'test' ? '0xb34C4319d007e0245B4715839d08f448fE42eA09' : '0x',
+    marketContractAddress: NETWORK === 'test' ? '0xb34C4319d007e0245B4715839d08f448fE42eA09' : '0x03488dbEdE125106d67216a32A1746fce7947132',
     providerOptions: {
       timeout: 30000, // ms
       headers: {
@@ -26,8 +26,8 @@ const chainEndpointMap = {
     },
   },
   polygon: {
-    endPoint: NETWORK === 'test' ? `wss://polygon-mumbai.g.alchemy.com/v2/${process.env.ZSH_POLYGON_API_KEY}` : 'wss://',
-    marketContractAddress: NETWORK === 'test' ? '0x11f46E8C1E2Ac861e662A62080ED6dC63041Ff94' : '0x',
+    endPoint: NETWORK === 'test' ? `wss://polygon-mumbai.g.alchemy.com/v2/${process.env.ZSH_POLYGON_API_KEY}` : `wss://polygon-mainnet.g.alchemy.com/v2/${process.env.ZSH_POLYGON_API_KEY}`,
+    marketContractAddress: NETWORK === 'test' ? '0x11f46E8C1E2Ac861e662A62080ED6dC63041Ff94' : '0x79B9279803c179220A6A78781554B6D62861d812',
     providerOptions: {
       timeout: 30000, // ms
       clientConfig: {
@@ -43,8 +43,8 @@ const chainEndpointMap = {
     },
   },
   bsc: {
-    endPoint: NETWORK === 'test' ? `wss://ws-nd-057-809-208.p2pify.com/${process.env.ZSH_BSC_API_KEY}` : 'wss://dex.binance.org/api/ws',
-    marketContractAddress: NETWORK === 'test' ? '0x23db713F8539aB8B18AC4A14794e4Ea66469936B' : '0x',
+    endPoint: NETWORK === 'test' ? `wss://ws-nd-057-809-208.p2pify.com/${process.env.ZSH_BSC_API_KEY}` : `wss://ws-nd-120-875-210.p2pify.com/${process.env.ZSH_BSC_API_KEY}`,
+    marketContractAddress: NETWORK === 'test' ? '0x23db713F8539aB8B18AC4A14794e4Ea66469936B' : '0x5D6be6B9179372ecC98b460A7C58bF292f67D9b6',
     providerOptions: {
       timeout: 30000, // ms
       clientConfig: {
