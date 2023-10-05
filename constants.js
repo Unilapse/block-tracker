2aa92170816bf11a3215c0b6c69fa4ee1e003b4a
jumyeong33 <jm01028250379@gmail.com>
2023-10-05 15:09:08 +0900

    [Modify] testnet,mainnet mapping condition to constant

--- constants.js
+++ constants.js
@@ -17,6 +17,18 @@ const NftAddress = {
   Zero: '0x0000000000000000000000000000000000000000',
 };
 
+const MarketContract = {
+  Klaytn: NETWORK === 'test' ? '0xb34C4319d007e0245B4715839d08f448fE42eA09' : '0x03488dbEdE125106d67216a32A1746fce7947132',
+  Polygon: NETWORK === 'test' ? '0x11f46E8C1E2Ac861e662A62080ED6dC63041Ff94' : '0x79B9279803c179220A6A78781554B6D62861d812',
+  Bsc: NETWORK === 'test' ? '0x23db713F8539aB8B18AC4A14794e4Ea66469936B' : '0x5D6be6B9179372ecC98b460A7C58bF292f67D9b6',
+};
+
+const SocketEndpoint = {
+  Klaytn: NETWORK === 'test' ? `wss://node-api.klaytnapi.com/v1/ws/open?chain-id=1001` : 'wss://node-api.klaytnapi.com/v1/ws/open?chain-id=8217',
+  Polygon: NETWORK === 'test' ? `wss://polygon-mumbai.g.alchemy.com/v2/${process.env.ZSH_POLYGON_API_KEY}` : `wss://polygon-mainnet.g.alchemy.com/v2/${process.env.ZSH_POLYGON_API_KEY}`,
+  Bsc: NETWORK === 'test' ? `wss://ws-nd-057-809-208.p2pify.com/${process.env.ZSH_BSC_API_KEY}` : `wss://ws-nd-120-875-210.p2pify.com/${process.env.ZSH_BSC_API_KEY}`,
+};
+
 const LogMessage = {
   /**
    * Success Message
@@ -52,4 +64,4 @@ const LogMessage = {
   },
 };
 
-module.exports = { Chain, NftAddress, LogMessage };
+module.exports = { Chain, NftAddress, LogMessage, MarketContract, SocketEndpoint };
--- src/provider.js
+++ src/provider.js
@@ -1,15 +1,15 @@
-const NETWORK = process.env.ZSH_CHAIN;
 const Web3 = require('web3');
 const fs = require('fs');
 const marketContractABI = JSON.parse(fs.readFileSync('../tokenSalesABI', 'utf8'));
 const { writeErrorLogConnection } = require('./logAction');
+const { MarketContract, SocketEndpoint } = require('../constants');
 
 const chainEndpointMap = {
   klaytn: {
-    endPoint: NETWORK === 'test' ? 'wss://node-api.klaytnapi.com/v1/ws/open?chain-id=1001' : 'wss://node-api.klaytnapi.com/v1/ws/open?chain-id=8217',
-    marketContractAddress: NETWORK === 'test' ? '0xb34C4319d007e0245B4715839d08f448fE42eA09' : '0x03488dbEdE125106d67216a32A1746fce7947132',
+    endPoint: SocketEndpoint.Klaytn,
+    marketContractAddress: MarketContract.Klaytn,
     providerOptions: {
-      timeout: 30000, // ms
+      timeout: 50000, // ms
       headers: {
         authorization: `Basic ${process.env.ZSH_KAS_AUTH}`,
       },
@@ -26,8 +26,8 @@ const chainEndpointMap = {
     },
   },
   polygon: {
-    endPoint: NETWORK === 'test' ? `wss://polygon-mumbai.g.alchemy.com/v2/${process.env.ZSH_POLYGON_API_KEY}` : `wss://polygon-mainnet.g.alchemy.com/v2/${process.env.ZSH_POLYGON_API_KEY}`,
-    marketContractAddress: NETWORK === 'test' ? '0x11f46E8C1E2Ac861e662A62080ED6dC63041Ff94' : '0x79B9279803c179220A6A78781554B6D62861d812',
+    endPoint: SocketEndpoint.Polygon,
+    marketContractAddress: MarketContract.Polygon,
     providerOptions: {
       timeout: 30000, // ms
       clientConfig: {
@@ -43,8 +43,8 @@ const chainEndpointMap = {
     },
   },
   bsc: {
-    endPoint: NETWORK === 'test' ? `wss://ws-nd-057-809-208.p2pify.com/${process.env.ZSH_BSC_API_KEY}` : `wss://ws-nd-120-875-210.p2pify.com/${process.env.ZSH_BSC_API_KEY}`,
-    marketContractAddress: NETWORK === 'test' ? '0x23db713F8539aB8B18AC4A14794e4Ea66469936B' : '0x5D6be6B9179372ecC98b460A7C58bF292f67D9b6',
+    endPoint: SocketEndpoint.Bsc,
+    marketContractAddress: MarketContract.Bsc,
     providerOptions: {
       timeout: 30000, // ms
       clientConfig: {
@@ -80,7 +80,7 @@ function connectWebSocekt(provider, chain) {
       console.log(event);
       console.log('Websocket closed. Reconnecting...');
       writeErrorLogConnection(event, chain);
-      setTimeout(connect, 1000);
+      setTimeout(() => connect(), 1000);
     });
 
     provider.on('error', (error) => {
